3dfbd98b7c899554d77be6c165c3fbfbc7e82f8d
Josep de Cid <josep.de.cid@gmail.com>
2019-11-13 15:15:09 +0100

    Add GAN G and D

---
+++ src/models/gan/generator.py
@@ -0,0 +1,14 @@
+from torch import nn
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
+
+    @staticmethod
+    def noise():
+        pass
