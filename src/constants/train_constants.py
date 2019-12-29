9fc63dbcc531670413852f413be3d5694bb6d5b4
Josep de Cid <josep.de.cid@gmail.com>
2019-12-29 19:32:50 +0100

    Set scratch model for GA

--- src/constants/train_constants.py
+++ src/constants/train_constants.py
@@ -1,7 +1,7 @@
 import torch
 
 EPOCHS = 10
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 Z_SIZE = 100
 SHUFFLE_TRAIN = True
 
--- src/models/evolutionary/face_classifier.py
+++ src/models/evolutionary/face_classifier.py
@@ -1,25 +1,41 @@
-import torch
 from torch import nn
-from torch.nn import functional as F
-from torchvision.models import resnet18
 
 
 class FaceClassifier(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.model = resnet18(pretrained=True, progress=True)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(negative_slope=0.2),
 
-        # Freeze all layers
-        for child in self.model.children():
-            for param in child.parameters():
-                param.requires_grad = False
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(negative_slope=0.2),
 
-        self.classifier = nn.Linear(1000, 1)
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.MaxPool2d(kernel_size=2),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(negative_slope=0.2),
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(in_features=33856, out_features=1000),
+            nn.ReLU(),
+
+            nn.Linear(in_features=1000, out_features=100),
+            nn.ReLU(),
+
+            nn.Linear(in_features=100, out_features=1),
+            nn.Softmax(dim=0)
+        )
 
     def forward(self, x):
-        x = self.model(x)
-        x = F.relu(x)
-        x = self.classifier(x)
-        x = torch.softmax(x, dim=1)
+        b_size = x.size(0)
+        x = self.conv(x)
+        x = x.view(b_size, -1)
+        x = self.linear(x)
         return x.view(-1)
