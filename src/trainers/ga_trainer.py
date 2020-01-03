bdaf8659885ddcef78549417e10c10a77741ddfd
Josep de Cid <josep.de.cid@gmail.com>
2020-01-03 12:36:37 +0100

    Set validaton score for GA training

--- src/trainers/ga_trainer.py
+++ src/trainers/ga_trainer.py
@@ -1,14 +1,15 @@
 import os
+
 from torch import nn, optim
 
 from constants.train_constants import *
-from dataset.FaceNoFaceDataset import FaceNoFaceDataset
+from dataset.UTKFaceDataset import UTKFaceDataset
 from models.evolutionary.face_classifier import FaceClassifier
 from trainers.trainer import Trainer
 
 
 class GATrainer(Trainer):
-    def __init__(self, model: FaceClassifier, dataset: FaceNoFaceDataset, log_tag: str):
+    def __init__(self, model: FaceClassifier, dataset: UTKFaceDataset, log_tag: str):
         super().__init__(dataset, log_tag)
 
         self.model = model
@@ -17,24 +18,42 @@ class GATrainer(Trainer):
         self.optim = optim.Adam(params=self.model.parameters(), lr=0.001)
 
     def _run_batch(self, images: torch.Tensor, labels: torch.Tensor = None, iteration: int = 0) -> None:
+        self.model.train()
         self.model.zero_grad()
 
         noise = torch.rand(size=(images.size(0),)) * 2 - 1
-        images += torch.tensordot(a=noise.view(-1, 1),
-                                  b=torch.rand(size=images.size()),
-                                  dims=1).to(DEVICE)
+        train_images = images + torch.tensordot(a=noise.view(-1, 1),
+                                                b=torch.rand(size=images.size()),
+                                                dims=1).to(DEVICE)
         labels = (torch.ones_like(noise) - torch.abs(noise)).to(DEVICE)
 
-        pred = self.model(images)
+        pred = self.model(train_images)
         loss = self.criterion(pred, labels)
 
         loss.backward()
         self.optim.step()
 
-        self.writer.add_scalar(f'({self.log_tag}) Loss', loss, iteration)
         # self.writer.add_image(f'Img_{labels[0].cpu()}',
         #                       (images[0].cpu().detach() + 1) / 2,
         #                       global_step=iteration)
+
+        # Set Network in evaluation mode and calculate validation loss
+        # with the same images applying a different random noise.
+        self.model.eval()
+        with torch.no_grad():
+            noise = torch.rand(size=(images.size(0),)) * 2 - 1
+            val_images = images + torch.tensordot(a=noise.view(-1, 1),
+                                                  b=torch.rand(size=images.size()),
+                                                  dims=1).to(DEVICE)
+            labels = (torch.ones_like(noise) - torch.abs(noise)).to(DEVICE)
+
+            pred = self.model(val_images)
+            val_loss = self.criterion(pred, labels)
+
+        # Log loss values in Tensorboard.
+        self.writer.add_scalars('Loss values',
+                                {'Train': loss, 'Validation': val_loss},
+                                global_step=iteration)
 
     def _init_model(self):
         # Send network to the corresponding device (GPU or CPU)
