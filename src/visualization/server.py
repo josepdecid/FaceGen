874ac8008234ef03f2cbeeddd05ca80ef8b89419
Josep de Cid <josep.de.cid@gmail.com>
2020-01-05 12:37:22 +0100

    Set dynamic image without saving

--- src/visualization/server.py
+++ src/visualization/server.py
@@ -1,12 +1,13 @@
+import base64
 import glob
 import os
+from io import BytesIO
 
-import torch
 import numpy as np
+import torch
 from PIL import Image
 from dotenv import load_dotenv
-from flask import Flask, render_template, send_file
-from six import StringIO
+from flask import Flask, render_template
 
 from models.autoencoder.vae import VAE
 from utils.train_constants import Z_SIZE, DEVICE
@@ -27,15 +28,18 @@ def generate_sample():
         fake_image = 255 * ((fake_image + 1) / 2)
         fake_image = fake_image.squeeze().permute(1, 2, 0).numpy()
         fake_image = Image.fromarray(fake_image.astype(np.uint8))
-        fake_image.save('/Users/josepdecidrodriguez/Projects/MAI-CI-Project/src/visualization/static/sample.jpg')
+
+        buffered = BytesIO()
+        fake_image.save(buffered, format='PNG')
+        return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
 
 @app.route('/')
 def home():
     checkpoints_names = glob.glob(os.path.join(os.environ['CKPT_DIR'], '*.pt'))
     checkpoints_names = list(map(lambda x: 'Model ' + x.split(os.sep)[-1][:-3], checkpoints_names))
-    generate_sample()
-    return render_template('index.html', options=checkpoints_names)
+    img = generate_sample()
+    return render_template('index.html', options=checkpoints_names, img=img)
 
 
 if __name__ == '__main__':
